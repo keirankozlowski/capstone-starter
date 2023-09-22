77cb3ec21f582059c5413e819968f393499ea003
Christina Lee <chrlee93@gmail.com>
2023-09-22 13:38:54 -0700

    adding map markers

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
+import React from "react";
+import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
+import MapMarkers from "./MapMarkers";
+
 const mapStyles = {
-  width: '100%',
-  height: '100vh',
+  width: "100%",
+  height: "100vh",
 };
 const center = {
-  lat: 40.730610,
+  lat: 40.73061,
   lng: -73.935242,
 };
 const apiKey = import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY; // Define the API key
@@ -13,12 +15,10 @@ const Map = () => {
   return (
     <div>
       <LoadScript googleMapsApiKey={apiKey}>
-        <GoogleMap
-          mapContainerStyle={mapStyles}
-          zoom={14}
-          center={center}
-        >
+        <GoogleMap mapContainerStyle={mapStyles} zoom={14} center={center}>
           <Marker position={center} />
+
+          <MapMarkers />
         </GoogleMap>
       </LoadScript>
     </div>
