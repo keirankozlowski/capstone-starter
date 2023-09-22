c39a6e33d38d13dec00b2e399b2995007b06c5dd
Christina Lee <chrlee93@gmail.com>
2023-09-22 12:02:08 -0700

    OAuth implementation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+const clientId = import.meta.env.VITE_REACT_APP_CLIENT_ID;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId={clientId}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
