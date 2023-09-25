efea1308a8735a9d4e094a31b2ef9c9fad3d6655
Patsy O'Brien <97324605+patsylin@users.noreply.github.com>
2023-09-24 19:29:45 -0500

    first commit

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,4 +1,19 @@
 // drop tables
+async function dropTables() {
+    try {
+      // Drop existing tables if they exist
+      await client.query("DROP TABLE IF EXISTS user");
+      await client.query("DROP TABLE IF EXISTS pregnancy");
+      await client.query("DROP TABLE IF EXISTS pregnancyWeeks");
+      await client.query("DROP TABLE IF EXISTS weeks");
+
+      console.log("Tables dropped successfully");
+    } catch (error) {
+      console.error("Error dropping tables");
+      throw error;
+    }
+  }
+
 
 // create tables
 
