d427eb21c80fd11671f55a67abefb96c9674c27e
Christina Lee <chrlee93@gmail.com>
2023-09-25 10:54:37 -0700

    get all museums working

--- client/src/components/Museums/GetAllMuseums.jsx
+++ client/src/components/Museums/GetAllMuseums.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { fetchAllMuseums } from "../../helpers/fetching";
 
 export default function GetAllMuseums() {
+  const [museums, setMuseums] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const renderMuseums = async () => {
+      try {
+        const museumArray = await fetchAllMuseums();
+        console.log("Museum Array: ", museumArray);
+        setMuseums(museumArray);
+        return museumArray;
+      } catch (error) {
+        setError("Failed to fetch museums. Please try again later.");
+      }
+    };
+    renderMuseums();
+  }, []);
+
   return (
     <>
       <h1>Get All Museums</h1>
+
+      {museums.map((museum) => (
+        <div key={museum.museumId}>
+          <p>{museum.museumName}</p>
+        </div>
+      ))}
     </>
   );
 }
