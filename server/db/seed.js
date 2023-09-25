57d081dbd32b105dbb954ac666e6eb68c0fefe09
Patsy O'Brien <97324605+patsylin@users.noreply.github.com>
2023-09-24 19:48:49 -0500

    seed file update

--- server/db/seed.js
+++ server/db/seed.js
@@ -16,7 +16,63 @@ async function dropTables() {
 
 
 // create tables
+async function createTables() {
+    try {
+      await client.query(`
+        CREATE TABLE users (
+        "username" SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL,
+    );
+        CREATE TABLE pregnancy (
+        "pregnancy" SERIAL PRIMARY KEY,
+        "userId" INTEGER REFERENCES users("userId") NOT NULL,
+        "age" INTEGER REFERENCES users("user_id") NOT NULL
+
+    );
+          CREATE TABLE pregnancyWeeks (
+              "weekId" SERIAL PRIMARY KEY,
+              "userId" INTEGER REFERENCES users("userId") NOT NULL,
+              "pregnancyId" INTEGER REFERENCES posts("pregnancy") NOT NULL,
+
+          );
+          CREATE TABLE weeks (
+            weight varchar(255) NOT NULL
+            size varchar(255) NOT NULL
+            info varchar(255) NOT NULLs
+        );
+      `);
+      console.log("Tables created successfully");
+    } catch (error) {
+      console.error("Error creating tables:", error);
+    }
 
 // create initial items
 
 // rebuild db
+
+//Call all my functions and 'BUILD' my database
+const rebuildDb = async () => {
+    try {
+        //ACTUALLY connect to my local database
+        client.connect()
+        //Run our functions
+        await dropTables()
+        await createTables()
+
+        //Generating starting data
+        // console.log("starting to seed...")
+        // await createInitialUsers()
+        // await createInitialPosts()
+        // await createInitialComment()
+        // await createInitialLike()
+
+    } catch (error) {
+        console.error(error)
+    } finally {
+        //close our connection
+        client.end()
+    }
+  }
+
+  rebuildDb()
