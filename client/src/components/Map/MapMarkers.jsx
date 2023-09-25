24cae1d187238796e7d28fefdb4d38b504686a1e
Jennifer <jennifer.y.hong@gmail.com>
2023-09-25 10:28:53 -0400

    map markers

--- client/src/components/Map/MapMarkers.jsx
+++ client/src/components/Map/MapMarkers.jsx
@@ -1,16 +1,41 @@
+import React from "react";
 import { Marker } from "@react-google-maps/api";
 
-export default function MapMarkers() {
+const markers = [
+  {
+    position: { lat: 40.779434, lng: -73.963402 },
+    title: "The Metropolitan Museum of Art",
+  },
+  {
+    position: { lat: 40.782951, lng: -73.958992 },
+    title: "The Guggenheim Museum",
+  },
+  {
+    position: { lat: 40.761509, lng: -73.978271 },
+    title: "The MoMa",
+  },
+  {
+    position: { lat: 40.739613, lng: -74.008980 },
+    title: "The Whitney Museum of American Art",
+  },
+  {
+    position: { lat: 40.781303, lng: -73.974113 },
+    title: "American Museum of Natural History",
+  },
+]
+
+const MapMarkers = () => {
   return (
     <>
-      <Marker
-        position={{ lat: 40.73061, lng: -73.935242 }}
-        icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
-      />
-      <Marker
-        position={{ lat: 40.779434, lng: -73.963402 }}
-        icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
-      />
+      {markers.map((marker, index) => (
+        <Marker
+          key={index}
+          position={marker.position}
+          title={marker.title}
+        />
+      ))}
     </>
   );
-}
+};
+
+export default MapMarkers;
