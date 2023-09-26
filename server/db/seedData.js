e984c007eeffe08e752867ecae3fe5ffcce45662
Patsy O'Brien <97324605+patsylin@users.noreply.github.com>
2023-09-26 14:47:41 -0500

    backend set up

--- server/db/seedData.js
+++ server/db/seedData.js
@@ -1,1 +1,8 @@
 // dummy data in here
+const users = [
+    { username: 'Gustavo', password: 12345678 },
+    { username: 'Kieran', password: 87654321 },
+    { username: 'Patsy', password: 24681012 },
+  ]
+
+  module.exports = { users }
