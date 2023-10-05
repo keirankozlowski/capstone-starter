dd169ca383131beff23a503157442ca27816bb9d
Jennifer <jennifer.y.hong@gmail.com>
2023-10-05 12:48:29 -0400

    home page slideshow

--- client/src/components/Navigation/Home.jsx
+++ client/src/components/Navigation/Home.jsx
@@ -3,12 +3,22 @@ import React from "react";
 export default function Home() {
   return (
     <div className="home">
-      <h1 className="quote">“A visit to a museum is a search for beauty, truth, and meaning in our lives. Go to museums as often as you can.”
-– Maira Kalman</h1>
+      <h1 className="quote">
+        “A visit to a museum is a search for beauty, truth, and meaning in our
+        lives. Go to museums as often as you can.” – Maira Kalman
+      </h1>
+
+      <ul className="cb-slideshow">
+        <li>
+          <span>Image 01</span>
+        </li>
+        <li>
+          <span>Image 02</span>
+        </li>
+        <li>
+          <span>Image 03</span>
+        </li>
+      </ul>
     </div>
   );
 }
-
-
-
-
