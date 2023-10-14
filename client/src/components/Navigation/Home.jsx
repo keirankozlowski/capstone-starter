c2a79e2d5adba64acb4d948d2b8ad296c1151b38
Christina Lee <chrlee93@gmail.com>
2023-10-13 18:14:30 -0700

    adding register link to home page

--- client/src/components/Navigation/Home.jsx
+++ client/src/components/Navigation/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import homeVideo from "../Images/homepage.mp4";
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="home">
       <div className="video-background">
@@ -14,6 +17,15 @@ export default function Home() {
         “A visit to a museum is a search for beauty, truth, and meaning in our
         lives. Go to museums as often as you can.” – Maira Kalman
       </h1>
+
+      <button
+        onClick={() => {
+          navigate("/register");
+        }}
+        className="home-btn"
+      >
+        <span>New here? Create an account!</span>
+      </button>
     </div>
   );
 }
